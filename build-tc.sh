xyz-prjkt/llvmTC

build-tc: Build with LTO && PGO
Change-Id: I196062dd5726a6c2c65e54c79d52a821458c582c
Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

build-tc.sh
@@ -49,6 +49,8 @@ tg_post_msg "<code>Building LLVM</code>"
 	--targets "ARM;AArch64" \
 	--shallow-clone \
 	--incremental \
+	--pgo \
+	--lto full \
 	--build-type "Release" 2>&1 | tee build.log
 
 # Check if the final clang binary exists or not.

a3-Prjkt/a3-llvmbuild

Signed-off-by: Disconnnected0 <womhateui@gmail.com>

build-tc.sh
@@ -16,6 +16,8 @@ msg "Building LLVM..."
 	--shallow-clone \
 	--incremental \
 	--build-type "Release" \
+	--pgo \
+	--lto thin \
 
 # Build binutils
 msg "Building binutils..."