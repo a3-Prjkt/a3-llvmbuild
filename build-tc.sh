84c838f1ba6f741595216840b026422eb4e99d09
Disconnnected0 <womhateui@gmail.com>
2022-07-10 03:34:46 +0000

    Revert "build-tc: Integrate TG notifications and push to Github"
    This reverts commit 41c0fd79591178c6e9e6be81783685a74b72b793.

--- build-tc.sh
+++ build-tc.sh
@@ -36,21 +36,9 @@ fi
 NAME="Æ3-Clang"
 
 # Function to show an informational message
-msg() {
+function msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
-
-err() {
-    echo -e "\e[1;41m$*\e[0m"
-}
-
-# Set a directory
-DIR="$(pwd ...)"
-
-# Build Info
-rel_date="$(date "+%Y%m%d")" # ISO 8601 format
-rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
-builder_commit="$(git rev-parse HEAD)"
 
 # Build LLVM
 msg "Building LLVM for $NAME..."
@@ -61,12 +49,6 @@ msg "Building LLVM for $NAME..."
 	--shallow-clone \
 	--incremental \
 	--build-type "Release" 2>&1 | tee build.log
-
-# Check if the final clang binary exists or not.
-[ ! -f install/bin/clang-1* ] && {
-	err "Building LLVM failed ! Kindly check errors !!"
-	exit 1
-}
 
 # Build binutils
 msg "Building binutils for $NAME..."
@@ -90,40 +72,5 @@ for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | gre
 	bin="${bin: : -1}"
 
 	echo "$bin"
-	patchelf --set-rpath "$DIR/install/lib" "$bin"
+	patchelf --set-rpath "$ORIGIN/../lib" "$bin"
 done
-
-# Release Info
-pushd llvm-project || exit
-llvm_commit="$(git rev-parse HEAD)"
-short_llvm_commit="$(cut -c-8 <<< "$llvm_commit")"
-popd || exit
-
-llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
-binutils_ver="$(ls | grep "^binutils-" | sed "s/binutils-//g")"
-clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
-
-echo "Finished Building $NAME"
-echo "Clang Version: $clang_version"
-echo "Binutils Version: $binutils_ver"
-echo "LLVM Commit: $llvm_commit_url"
-
-# Push to GitHub
-# Update Git repository
-#git config --global user.name "Panchajanya1999"
-#git config --global user.email "panchajanya@azure-dev.live"
-#git clone "https://Panchajanya1999:$GITHUB_TOKEN@github.com/Panchajanya1999/azure-clang.git" rel_repo
-#pushd rel_repo || exit
-#rm -fr ./*
-#cp -r ../install/* .
-#git checkout README.md # keep this as it's not part of the toolchain itself
-#git add .
-#git commit -asm "Update to $rel_date build
-
-#LLVM commit: $llvm_commit_url
-#Clang Version: $clang_version
-#Binutils version: $binutils_ver
-#Builder commit: https://github.com/Panchajanya1999/tc-build/commit/$builder_commit"
-#git push -f
-#popd || exit
-#tg_post_msg "<b>Toolchain Compilation Finished and pushed</b>"
