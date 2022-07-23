ee1ad5796cdaa357b4a5622265ccbb5c5781b401
Disconnnected0 <womhateui@gmail.com>
2022-07-23 01:47:57 +0000

    build-tc: build compiler-rt
    Signed-off-by: Disconnnected0 <womhateui@gmail.com>

--- build-tc.sh
+++ build-tc.sh
@@ -45,7 +45,7 @@ function msg() {
 msg "Building LLVM for $NAME..."
 ./build-llvm.py \
 	--clang-vendor "$NAME" \
-	--projects "clang;lld;polly" \
+	--projects "clang;lld;compiler-rt;polly" \
 	--targets "ARM;AArch64" \
 	--incremental 2>&1 | tee build.log 
 
